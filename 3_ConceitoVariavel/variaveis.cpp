18685e60fa7c2f1f18e4cb802574cf6dad645aee
dalins <danillo_lins@hotmail.com>
2023-05-22 17:16:01 -0300

    conceito variavel

---
+++ 3_ConceitoVariavel/variaveis.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <tchar.h>
+
+int main()
+{
+    _tsetlocale(LC_ALL, _T("portuguese"));
+    int NumVidas = 5;
+    int Score = 1350;
+
+    std::cout << "***************" << std::endl;
+    std::cout << "Vidas Jogador: " << NumVidas << std::endl;
+    std::cout << "Pontuacao: " << Score << std::endl;
+    std::cout << "***************" << std::endl;
+
+}
