6fb87364d86f3a40f9aaf2f0cc2891233ab8e3bf
dalins <danillo_lins@hotmail.com>
2023-05-23 10:06:05 -0300

    variaveis_2

--- 3_ConceitoVariavel/variaveis.cpp
+++ 3_ConceitoVariavel/variaveis.cpp
@@ -10,7 +10,16 @@ int main()
     std::cout << "***************" << std::endl;
     std::cout << "Vidas Jogador: " << NumVidas << std::endl;
     std::cout << "Pontuacao: " << Score << std::endl;
-    std::cout << "Endereço que Numvidas Ocupa na memoria RAM: " << &NumVidas << std::endl;
+    std::cout << "Endereco que Numvidas Ocupa na memoria RAM: " << &NumVidas << std::endl;
+    std::cout << "Endereco que Score Ocupa na memoria RAM: " << &Score << std::endl;
     std::cout << "***************" << std::endl;
+
+    std::cout << "*******DURANTE O JOGO********" << std::endl;
+    Score += 150;
+    NumVidas -= 1;
+    std::cout << "Vidas Jogador: " << NumVidas << std::endl;
+    std::cout << "Pontuacao: " << Score << std::endl;
+
+
 
 }
